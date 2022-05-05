bb3c41a34d8e528ec763c3561d03728ddbff90c1
Eric Kelley <hello@ericmkelley.com>
2022-05-05 10:17:27 -0400

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@
           elegant web applications that customers love to use. Currently, I’m
           currently helping software development stay human at
           <a href="https://dencap.com/" target="_blank" rel="noreferrer">
-            DENCAP Dental.
+            DENCAP Dental Plans.
           </a>
         </p>
         <br />
