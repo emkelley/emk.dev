322f631ecd5718e4ff9c528f5cef615bc6faeef0
Eric Kelley <hello@ericmkelley.com>
2023-07-03 17:44:47 -0400

    switch fathom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,17 +7,8 @@ export default defineNuxtConfig({
         {
           type: "text/javascript",
           innerHTML: `
-          (function(f, a, t, h, o, m){
-            a[h]=a[h]||function(){
-              (a[h].q=a[h].q||[]).push(arguments)
-            };
-            o=f.createElement('script'),
-            m=f.getElementsByTagName('script')[0];
-            o.async=1; o.src=t; o.id='fathom-script';
-            m.parentNode.insertBefore(o,m)
-          })(document, window, '//stats.emk.dev/tracker.js', 'fathom');
-          fathom('set', 'siteId', 'LWCOI');
-          fathom('trackPageview');`,
+                <script src="https://cdn.usefathom.com/script.js" data-site="YRVLFDYM" defer></script>
+          `,
         },
       ],
     },
