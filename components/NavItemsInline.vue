885bf3a2837a083c1de4e20c5fe9dbd9b371df3d
Eric Kelley <hello@ericmkelley.com>
2023-07-08 09:29:19 -0400

    .

--- components/NavItemsInline.vue
+++ components/NavItemsInline.vue
@@ -5,7 +5,7 @@ const active_route = computed(() => route.path);
 </script>
 <template>
   <nav
-    class="w-full py-4 flex items-center justify-center px-12 sticky top-0 bg-[#0f172a]/60 backdrop-blur-md z-[999] rounded-b-xl overflow-hidden"
+    class="w-full py-4 flex items-center justify-center px-4 md:px-12 sticky top-0 bg-[#0f172a]/60 backdrop-blur-md z-[999] rounded-b-xl overflow-hidden"
   >
     <div class="bg-[#1c2b50] flex text-white rounded-md shadow-md p-3 gap-6">
       <nuxt-link to="/" :class="active_route === '/' ? 'nav_active' : ''">
@@ -24,7 +24,7 @@ const active_route = computed(() => route.path);
         Uses
       </nuxt-link>
       <a href="https://ek-webtools.netlify.app/" target="_blank">
-        Web Tools <Icon name="ic:round-launch" class="ml-2" />
+        Tools <Icon name="ic:round-launch" class="ml-1" />
       </a>
     </div>
   </nav>
